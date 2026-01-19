4b39520be26917daf3c62c8694fa83684951de48
grigan-tps <ylanheb@gmail.com>
2026-01-19 15:22:18 +0100

    Fix backend setup
    t Last command done (1 command done):

--- backend/Database/Migrations/InitialDbSetup.cs
+++ backend/Database/Migrations/InitialDbSetup.cs
@@ -8,7 +8,9 @@ public class InitialDbSetup : Migration {
     public override void Up() {
         Create.Table("users")
             .WithColumn("id").AsInt32().PrimaryKey().Identity()
-            .WithColumn("username").AsString(100).NotNullable()
+            .WithColumn("username").AsString(100).Nullable()
+            .WithColumn("email").AsString(255).Nullable()
+            .WithColumn("password_hash").AsString(255).NotNullable()
             .WithColumn("profile").AsInt16().NotNullable().WithDefaultValue((int)ProfileType.Student)
             .WithColumn("created_at").AsDateTime().WithDefault(SystemMethods.CurrentUTCDateTime);
     }
