c555795b541b39fcc5e97a558d376139af540db2
Looki-fr <lemeilllouis@gmail.com>
2026-01-22 11:02:41 +0100

    home roouter for different types of users

---
+++ frontend/src/app/pages/HomeParent/home-parent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-home-parent',
+    templateUrl: './home-parent.component.html',
+    styleUrls: ['./home-parent.component.css']
+})
+export class HomeParentComponent {
+    constructor() {}
+}
